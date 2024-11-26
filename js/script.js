2c7939d312ad17da774a2be24582ae122f510401
Jonathan Sartori Bruzarrosco <jonathansartorib@hotmail.com>
2024-11-26 15:55:19 -0300

    Update Site

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const btnCoffee1 = document.querySelector("#addCoffee1");
+const btnCoffee2 = document.querySelector("#addCoffee2");
+
+btnCoffee1.addEventListener("click", function(event) {
+    
+	event.preventDefault(); 
+
+    alert("SITE EM CONSTRUÇÂO, DESCULPE PELO TRANSTORNO")
+});
+
+btnCoffee2.addEventListener("click", function(event) {
+    
+	event.preventDefault(); 
+
+    alert("SITE EM CONSTRUÇÂO, DESCULPE PELO TRANSTORNO")
+});
